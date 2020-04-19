4e34921b7d5fd6b3bbbd1befa7543bf01456455b
nima <nimxj4141@gmail.com>
2020-04-19 23:54:11 +0430

    third feature layout added

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -6,13 +6,51 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private Switch heavySleepSwitch;
+    private Switch vibrationSwitch;
+    private Switch sleepSwitch;
+    private Button heavySleepButton;
+    private Button vibrationButton;
+    private Button sleepButton;
+    private double sleepCriticalAngle = 0;
+    private EditText angleText;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+
+    }
+
+    public void initialize() {
+        heavySleepSwitch = findViewById(R.id.switch1);
+        vibrationSwitch = findViewById(R.id.switch2);
+        sleepSwitch = findViewById(R.id.switch3);
+        heavySleepButton = findViewById(R.id.first);
+        vibrationButton = findViewById(R.id.second);
+        sleepButton = findViewById(R.id.third);
+
+        sleepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.sleep_layout);
+                sleepActivity();
+            }
+        });
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
     }
 
     @Override
@@ -20,8 +58,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    public void sleepActivity() {
+        angleText = findViewById(R.id.angle);
+        angleText.setText(String.valueOf(sleepCriticalAngle));
+        saveButton = findViewById(R.id.save);
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sleepCriticalAngle = Double.parseDouble(angleText.getText().toString());
+                setContentView(R.layout.activity_main);
+                initialize();
+            }
+        });
     }
 }
