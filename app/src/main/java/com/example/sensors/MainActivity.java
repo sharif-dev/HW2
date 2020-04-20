98f961abee279418a0cd35ac524d019878a637de
sina-kz <sinakazemi1998@gmail.com>
2020-04-20 14:48:30 +0430

    some changes

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.sensors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -18,19 +18,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Button heavySleepButton;
     private Button vibrationButton;
     private Button sleepButton;
-    private double sleepCriticalAngle = 0;
-    private EditText angleText;
-    private Button saveButton;
+    private double sleepCriticalAngle = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialize();
-
-    }
-
-    public void initialize() {
         heavySleepSwitch = findViewById(R.id.switch1);
         vibrationSwitch = findViewById(R.id.switch2);
         sleepSwitch = findViewById(R.id.switch3);
@@ -40,13 +33,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sleepButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                setContentView(R.layout.sleep_layout);
-                sleepActivity();
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, sleep_activity.class));
             }
         });
-    }
 
+    }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -56,20 +48,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
-    }
-
-    public void sleepActivity() {
-        angleText = findViewById(R.id.angle);
-        angleText.setText(String.valueOf(sleepCriticalAngle));
-        saveButton = findViewById(R.id.save);
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sleepCriticalAngle = Double.parseDouble(angleText.getText().toString());
-                setContentView(R.layout.activity_main);
-                initialize();
-            }
-        });
     }
 }
