51b326500ee29153f3e04223afb4287da5ca19e6
sina-kz <sinakazemi1998@gmail.com>
2020-04-29 17:53:12 +0430

    layout completed

---
+++ app/src/main/java/com/example/sensors/heavySleepActivity.java
@@ -0,0 +1,14 @@
+package com.example.sensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class heavySleepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heavy_sleep);
+    }
+}
