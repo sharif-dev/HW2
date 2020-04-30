968be39ba087949d61a8fc71f470ff67e0b8760b
sina-kz <sinakazemi1998@gmail.com>
2020-04-30 10:52:34 +0430

    Alarm receiver

--- app/src/main/java/com/example/sensors/heavySleepActivity.java
+++ app/src/main/java/com/example/sensors/heavySleepActivity.java
@@ -3,7 +3,9 @@ package com.example.sensors;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlarmManager;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class heavySleepActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
     private TextView time;
@@ -43,10 +48,37 @@ public class heavySleepActivity extends AppCompatActivity implements TimePickerD
                 timePicker.show(getSupportFragmentManager(), "time picker");
             }
         });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+    }
+
+    private void cancelAlarm() {
+        // TODO: 4/30/20  
     }
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        time.setText("Hour " + hourOfDay + " Minute: " + minute);
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+
+        updateTimeText(c);
+        startAlarm(c);
+    }
+
+    private void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        // TODO: 4/30/20
+    }
+
+    private void updateTimeText(Calendar c) {
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+        time.setText(timeText);
     }
 }
