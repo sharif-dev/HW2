b188cee7f445cab1b75b234f1dc53caff8178d23
Sepehr Feli <sepehr.feli.1377@gmail.com>
2020-05-02 01:28:24 +0430

    shakeService created

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -10,10 +10,13 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import com.example.sensors.Shake.shakeService;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private Intent intent1;
 
 
+    private Intent intent2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
         vibrationButton = findViewById(R.id.second);
         sleepButton = findViewById(R.id.third);
         intent1 = new Intent(MainActivity.this, SleepService.class);
+
+
+
+        intent2 = new Intent(MainActivity.this, shakeService.class);
+
+
+
         final SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         final SharedPreferences.Editor editor = sharedPreferences.edit();
 
@@ -72,11 +85,47 @@ public class MainActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
-        vibrationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        vibrationSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                editor.putBoolean("SWITCH2", isChecked);
+            public void onClick(View view) {
+                editor.putBoolean("SWITCH2", vibrationSwitch.isChecked());
                 editor.apply();
+                if (!vibrationSwitch.isChecked()) {
+                    devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+                    try {
+                        componentName = new ComponentName(MainActivity.this, DeviceAdmin.class);
+                        boolean active = devicePolicyManager.isAdminActive(componentName);
+                        if (active) {
+                            devicePolicyManager.removeActiveAdmin(componentName);
+                            stopService(intent2);
+                        } else {
+                            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+                            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "You should enable the app!");
+                            stopService(intent2);
+                        }
+                    } catch (NullPointerException e) {
+                        e.printStackTrace();
+                    }
+
+
+                } else {
+                    devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+                    try {
+                        componentName = new ComponentName(MainActivity.this, DeviceAdmin.class);
+                        boolean active = devicePolicyManager.isAdminActive(componentName);
+                        if (active) {
+                            devicePolicyManager.removeActiveAdmin(componentName);
+                        } else {
+                            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+                            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "You should enable the app!");
+                            startActivityForResult(intent, 12);
+                        }
+                    } catch (NullPointerException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
         sleepSwitch.setOnClickListener(new View.OnClickListener() {
@@ -122,7 +171,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
 
 
@@ -137,6 +185,16 @@ public class MainActivity extends AppCompatActivity {
                 startService(intent1);
             } else {
                 sleepSwitch.setChecked(false);
+            }
+        }else if (requestCode == 12) {
+            if (resultCode == Activity.RESULT_OK) {
+                vibrationSwitch.setChecked(true);
+                shakeService.mDevicePolicyManager = devicePolicyManager;
+                shakeService.mComponentName = componentName;
+                startService(intent2);
+                Log.d("MMMMMMMMMMMMMMMMMMMM", "MainActivity");
+            } else {
+                vibrationSwitch.setChecked(false);
             }
         }
     }
