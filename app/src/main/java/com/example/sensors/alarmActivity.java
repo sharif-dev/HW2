662e405f669e60242359f5413823fac730d23c41
sina-kz <sinakazemi1998@gmail.com>
2020-05-01 10:48:15 +0430

    count down done

--- app/src/main/java/com/example/sensors/alarmActivity.java
+++ app/src/main/java/com/example/sensors/alarmActivity.java
@@ -4,17 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.provider.Settings;
+import android.widget.TextView;
 
 public class alarmActivity extends AppCompatActivity {
 
-    MediaPlayer player;
+    private MediaPlayer player;
+    private TextView time_txt;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliSeconds = 10000; //10min
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        time_txt = findViewById(R.id.time_text);
         player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
         player.start();
+        countDownTimer = new CountDownTimer(timeLeftInMilliSeconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMilliSeconds = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                player.release();
+            }
+        }.start();
+    }
+
+    private void updateTimer() {
+        int minutes = (int) timeLeftInMilliSeconds / 60000;
+        int seconds = (int) timeLeftInMilliSeconds % 60000 / 1000;
+
+        String timeLeftText;
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if (seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+
+        time_txt.setText(timeLeftText);
     }
 }
